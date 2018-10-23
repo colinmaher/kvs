f2b6a09a6b4198e5c51a7491d9cddab654c17e74
Colin <colinmaher123@gmail.com>
2018-10-23 15:40:30 -0700

    put adds key value pair to hash

--- api/routes/keyValue-store.js
+++ api/routes/keyValue-store.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const hash = {};
 
 router.put('/', (req, res, next) => {
 	var query = req.query;
 	//Error message working
+	console.log(Object.values(query).toString());
+	//convert to string
+	hash[Object.keys(query).toString()] = Object.values(query).toString();
+	console.log("hash: " + hash);
 	res.status(201).json({
 		'replaced': 'False',
-		'msg':'Added successfully',
-		'key':Object.keys(query),
-		'val':Object.values(query)
+		'msg': 'Added successfully',
+		'key': Object.keys(query),
+		'val': Object.values(query)
 	});
 })
 
@@ -16,7 +21,7 @@ router.get('/', (req, res, next) => {
 	//Error message working
 	res.status(404).json({
 		'result': 'Error',
-		'msg':'Not Found'
+		'msg': 'Not Found'
 	});
 })
 
@@ -25,7 +30,7 @@ router.delete('/', (req, res, next) => {
 	//Error message working
 	res.status(404).json({
 		'result': 'Error',
-		'msg':'Status code 404'
+		'msg': 'Status code 404'
 	});
 })
 
