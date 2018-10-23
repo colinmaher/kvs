61f7b41cc89172b5ed85d5929fc024620eea050e
Colin <colinmaher123@gmail.com>
2018-10-23 16:29:15 -0700

    get and delete working + added other put conditions + a string size function

--- api/routes/keyValue-store.js
+++ api/routes/keyValue-store.js
@@ -9,53 +9,74 @@ router.put('/', (req, res, next) => {
 	//Error message working
 	console.log("key: " + key + " value: " + value);
 	//convert to string
-	
-	if (key in hash) {
+
+	if (!keyCheck(key)) {
+		res.status(404).json({
+			'result': 'Error',
+			'msg': 'Key not valid'
+		});
+	} else if (valCheck(value)) {
+		res.status(404).json({
+			'result': 'Error',
+			'msg': 'Object too large. Size limit is 1MB'
+		});
+	} else if (key in hash) {
+		hash[key] = value;
 		res.status(201).json({
 			'replaced': 'True',
 			'msg': 'Added successfully',
 		});
 	} else {
-		res.status(201).json({
+		hash[key] = value;
+		res.status(200).json({
 			'replaced': 'False',
 			'msg': 'Added successfully',
 		});
 	}
-	hash[key] = value;
 	console.log("hash: " + hash);
-
 })
 
 router.get('/', (req, res, next) => {
 	//Error message working
-	res.status(404).json({
-		'result': 'Error',
-		'msg': 'Not Found'
-	});
+	const query = req.query;
+	const key = Object.keys(query).toString();
+
+	if (key in hash) {
+		res.status(200).json({
+			'result': 'Success',
+			'value': hash[key]
+		});
+	} else {
+		res.status(404).json({
+			'result': 'Error',
+			'msg': 'Not Found'
+		});
+	}
 })
 
 
 router.delete('/', (req, res, next) => {
+	const query = req.query;
+	const key = Object.keys(query).toString();
 	//Error message working
-	res.status(404).json({
-		'result': 'Error',
-		'msg': 'Status code 404'
-	});
+	if (key in hash) {
+		delete hash[key];
+		res.status(200).json({
+			'result': 'Success',
+		});
+	} else {
+		res.status(404).json({
+			'result': 'Error',
+			'msg': 'Status code 404'
+		});
+	}
 })
-
-
-
-
 
 //
 //Check if user input key & val are valid to be processed.
 function keyCheck(key) {
 	//If key is not alphanumeric OR empty
 	if (!key.match(/^[a-zA-Z0-9]+$/i) || key.trim() === '') {
-		res.status(404).json({
-			'result': 'Error',
-			'msg': 'Key not valid'
-		});
 		return false;
 	}
 	//Otherwise, everything is fine and return true
@@ -63,15 +84,24 @@ function keyCheck(key) {
 }
 //Check if value is less than
 function valCheck(val) {
-	var size = encodeURI(s).split(/%..|./).length - 1;
+	const size = byteLength(val);
 	console.log("Printing size: " + size);
 	if (size > 1000000) {
-		res.status(404).json({
-			'result': 'Error',
-			'msg': 'Object too large. Size limit is 1MB'
-		});
 		return false;
 	}
+}
+
+// https://gist.github.com/lovasoa/11357947
+function byteLength(str) {
+	// returns the byte length of an utf8 string
+	var s = str.length;
+	for (var i = str.length - 1; i >= 0; i--) {
+		var code = str.charCodeAt(i);
+		if (code > 0x7f && code <= 0x7ff) s++;
+		else if (code > 0x7ff && code <= 0xffff) s += 2;
+		if (code >= 0xDC00 && code <= 0xDFFF) i--; //trail surrogate
+	}
+	return s;
 }
 
 module.exports = router;
